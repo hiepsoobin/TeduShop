bfff95013fe8f4f113bc4e306f80885fc6643b74
DESKTOP-AD4JMMS\ADMIN <hiepsoobin361@gmail.com>
2023-10-30 13:05:20 +0700

    s

--- Tedushop/TeduShop.Web/Controllers/HomeController.cs
+++ Tedushop/TeduShop.Web/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace TeduShop.Web.Controllers
     {
         public ActionResult Index()
         {
-            return View();em
+            return View();rrrrr
         }
 
         public ActionResult About()
